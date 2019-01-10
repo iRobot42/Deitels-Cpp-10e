772bcbe7434b8543fdca9a0bfc171b27812b0f43
Alexander Kobets <32043436+iRobot42@users.noreply.github.com>
2019-01-10 22:22:44 +0300

    Create ex_02_26.cpp

---
+++ 02 - Itroduction to Cpp Programming/02.26 - Checkerboard Pattern/ex_02_26.cpp
@@ -0,0 +1,26 @@
+// Exercise 2.26: ex_02_26.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   // Eight statements
+   cout << "* * * * * * * *\n";
+   cout << " * * * * * * * *\n";
+   cout << "* * * * * * * *\n";
+   cout << " * * * * * * * *\n";
+   cout << "* * * * * * * *\n";
+   cout << " * * * * * * * *\n";
+   cout << "* * * * * * * *\n";
+   cout << " * * * * * * * *\n\n";
+
+   // One statement
+   cout << "* * * * * * * *\n * * * * * * * *\n"
+        << "* * * * * * * *\n * * * * * * * *\n"
+        << "* * * * * * * *\n * * * * * * * *\n"
+        << "* * * * * * * *\n * * * * * * * *\n" << endl;
+
+   return 0;
+}
