8f464e2b377e4c433bb952bfc769287528224522
Alexander Kobets <32043436+iRobot42@users.noreply.github.com>
2019-01-10 21:29:35 +0300

    Create ex_02_18.cpp

---
+++ 02 - Itroduction to Cpp Programming/02.18 - Comparing Integers/ex_02_18.cpp
@@ -0,0 +1,25 @@
+// Exercise 2.18: ex_02_18.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   int number1, number2;
+
+   cout << "Two numbers: ";
+   cin >> number1 >> number2;
+
+   if ( number1 == number2 ) {
+      cout << "These numbers are equal.\n";
+      return 0;
+   }
+
+   if ( number2 > number1 )
+      number1 = number2;
+
+   cout << number1 << " is larger." << endl;
+
+   return 0;
+}
