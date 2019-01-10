e542c9b4fbdba25ae0d3260a5cfb037d88025718
Alexander Kobets <32043436+iRobot42@users.noreply.github.com>
2019-01-10 22:11:11 +0300

    Create ex_02_20.cpp

---
+++ 02 - Itroduction to Cpp Programming/02.20 - Diameter Circumference and Area of a Circle/ex_02_20.cpp
@@ -0,0 +1,19 @@
+// Exercise 2.20: ex_02_20.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   int radius;
+
+   cout << "Integer radius of a circle: ";
+   cin >> radius;
+
+   cout << "Diameter is " << 2 * radius
+      << "\nCircumference is " << 2 * 3.14159 * radius
+      << "\nArea is " << 3.14159 * radius * radius << endl;
+
+   return 0;
+}
