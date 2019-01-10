470fbf2777c48de8199ae43ea0d87897f7504e40
Alexander Kobets <32043436+iRobot42@users.noreply.github.com>
2019-01-10 23:06:58 +0300

    Create ex_04_08.cpp

---
+++ 04 - Algorithm Development and Control Statements. Part I/04.08 - Untitled/ex_04_08.cpp
@@ -0,0 +1,28 @@
+// Exercise 4.08: ex_04_08.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   cout << "x: ";
+   unsigned int x;
+   cin >> x;
+
+   cout << "y: ";
+   unsigned int y;
+   cin >> y;
+
+   unsigned int i{ 1 };
+   unsigned int power{ 1 };
+
+   while ( i <= y ) {
+      power *= x;
+      ++i;
+   }
+
+   cout << "Power is " << power;
+
+   return 0;
+}
