94ef1d895fa59e22f4ab8510bfbc067e5bc09fbe
Alexander Kobets <32043436+iRobot42@users.noreply.github.com>
2019-01-10 22:13:28 +0300

    Create ex_02_21.cpp

---
+++ 02 - Itroduction to Cpp Programming/02.21 - Displaying Large Letters/ex_02_21.cpp
@@ -0,0 +1,13 @@
+// Exercise 2.21: ex_02_21.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   cout << "  CCC\t+\t+\n C\t+\t+\nC     +++++   +++++\n C\t+\t+\n  CCC\t+\t+"
+      << endl;
+
+   return 0;
+}
