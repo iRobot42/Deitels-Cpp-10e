1300782af47c62761135f375dc2c6a7bbd9ecd84
Alexander Kobets <32043436+iRobot42@users.noreply.github.com>
2019-01-10 21:26:05 +0300

    Create ex_02_16.cpp

---
+++ 02 - Itroduction to Cpp Programming/02.16 - Arithmetic/ex_02_16.cpp
@@ -0,0 +1,22 @@
+// Exercise 2.16: ex_02_16.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   int number1, number2;
+
+   cout << "Two numbers: ";
+   cin >> number1 >> number2;
+
+   cout << "\nSum is " << number1 + number2
+      << "\nProduct is " << number1 * number2
+      << "\nDifference is " << number1 - number2 << endl;
+
+   if ( number2 != 0 )
+      cout << "Quotient is " << number1 / number2 << endl;
+
+   return 0;
+}
