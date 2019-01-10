600225b394270f007c167e87af54e67873d2e473
Alexander Kobets <32043436+iRobot42@users.noreply.github.com>
2019-01-10 21:28:16 +0300

    Create ex_02_17.cpp

---
+++ 02 - Itroduction to Cpp Programming/02.17 - Printing/ex_02_17.cpp
@@ -0,0 +1,20 @@
+// Exercise 2.17: ex_02_17.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   // a
+   cout << "V X Y Z" << endl;
+
+   // b
+   cout << "V " << "X " << "Y " << "Z\n";
+
+   // c
+   cout << "V ";
+   cout << "X Y Z\n";
+
+   return 0;
+}
