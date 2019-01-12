f0917ce3a482c8cbf83e5b79b44f0828ed0a96c6
Alexander Kobets <32043436+iRobot42@users.noreply.github.com>
2019-01-12 20:04:02 +0300

    Create ex_06_03.cpp

---
+++ 06 - Functions and an Introduction to Recursion/06.03 - Untitled/ex_06_03.cpp
@@ -0,0 +1,33 @@
+// Exercise 6.3: ex_06_03.cpp
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   cout << "ceil( 9.2 ) is " << ceil( 9.2 )
+      << "\nceil( -9.8 ) is " << ceil( -9.8 )
+      << "\ncos( 0.0 ) is " << cos( 0.0 )
+      << "\nexp( 1.0 ) is " << exp( 1.0 )
+      << "\nexp( 2.0 ) is " << exp( 2.0 )
+      << "\nfabs( 5.1 ) is " << fabs( 5.1 )
+      << "\nfabs( 0.0 ) is " << fabs( 0.0 )
+      << "\nfabs( -8.76 ) is " << fabs( -8.76 )
+      << "\nfloor( 9.2 ) is " << floor( 9.2 )
+      << "\nfloor( -9.8 ) is " << floor( -9.8 )
+      << "\nfmod( 2.6, 1.2 ) is " << fmod( 2.6, 1.2 )
+      << "\nlog( 2.718282 ) is " << log( 2.718282 )
+      << "\nlog( 7.389056 ) is " << log( 7.389056 )
+      << "\nlog10( 10.0 ) is " << log10( 10.0 )
+      << "\nlog10( 100.0 ) is " << log10( 100.0 )
+      << "\npow( 2, 7 ) is " << pow( 2, 7 )
+      << "\npow( 9, .5 ) is " << pow( 9, .5 )
+      << "\nsin( 0.0 ) is " << sin( 0.0 )
+      << "\nsqrt( 9.0 ) is " << sqrt( 9.0 )
+      << "\ntan( 0.0 ) is " << tan( 0.0 )
+      << endl;
+
+   return 0;
+}
