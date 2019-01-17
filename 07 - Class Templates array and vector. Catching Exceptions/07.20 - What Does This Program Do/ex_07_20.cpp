b4bb939cbd797a5a6cbddc205557322a987d07d9
Alexander Kobez <alexander.kobez@gmail.com>
2019-01-18 01:39:55 +0300

    Create ex_07_20.cpp

---
+++ 07 - Class Templates array and vector. Catching Exceptions/07.20 - What Does This Program Do/ex_07_20.cpp
@@ -0,0 +1,32 @@
+// Exercise 7.20: ex_07_20.cpp
+
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+const size_t arraySize{ 10 };
+void someFunction( const array< int, arraySize >&, size_t );
+
+int main() {
+   
+   array< int, arraySize > a{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+   cout << "The values in the array are:" << endl;
+   someFunction( a, 0 );
+
+   cout << endl;
+}
+
+// What does this function do?
+void someFunction( const array< int, arraySize >& b, size_t current ) {
+   if ( current < b.size() ) {
+      someFunction( b, current + 1 );
+      cout << b[ current ] << " ";
+   }
+}
+
+// My answer:
+
+// This program displays elements of an array in reversed order.
+// Function "someFunction" does it recursively.
