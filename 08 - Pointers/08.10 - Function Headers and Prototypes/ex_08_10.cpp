c9bd8f389a08cca86d5f7a7bc4cc5465bfb7d7e0
Alexander Kobez <alexander.kobez@gmail.com>
2019-05-05 14:36:42 +0300

    Create ex_08_10.cpp

---
+++ 08 - Pointers/08.10 - Function Headers and Prototypes/ex_08_10.cpp
@@ -0,0 +1,9 @@
+// Exercise 8.10: ex_08_10.cpp
+
+void zero( long int[], const size_t ); // b
+int add1AndSum( int[], const size_t ); // d
+
+int main() {}
+
+void zero( long int bigIntegers[], const size_t SIZE ) {} // a
+int add1AndSum( int oneTooSmall[], const size_t SIZE ) { return 0; } // c
