e0834364b5e8ae97bd5f9bbdde6edde87aac1aed
Alexander Kobez <alexander.kobez@gmail.com>
2021-10-07 19:32:54 +0300

    Create ex_09_08.cpp

---
+++ 09 - Classes. A Deeper Look/09.08 - Student Class/ex_09_08.cpp
@@ -0,0 +1,13 @@
+// Exercise 9.8: ex_09_08.cpp
+// Student Class
+
+#include "Student.h"
+
+using namespace std;
+
+int main() {
+
+   Student s{ "John", "W.", "Smith", 'm', 2000, 12345, "john@mail.box" };
+
+   return 0;
+}
