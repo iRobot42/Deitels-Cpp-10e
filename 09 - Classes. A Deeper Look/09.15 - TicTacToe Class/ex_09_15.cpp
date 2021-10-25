6e45252a6becad06da264078f23fdb4a90a6425e
Alexander Kobez <alexander.kobez@gmail.com>
2021-10-25 12:07:23 +0300

    Create ex_09_15.cpp

---
+++ 09 - Classes. A Deeper Look/09.15 - TicTacToe Class/ex_09_15.cpp
@@ -0,0 +1,17 @@
+// Exercise 9.15: ex_09_15.cpp
+// TicTacToe Class
+
+#include <iostream>
+
+#include "TicTacToe.h"
+
+using namespace std;
+
+int main() {
+
+   TicTacToe Game;
+
+   Game.play();
+
+   return 0;
+}
