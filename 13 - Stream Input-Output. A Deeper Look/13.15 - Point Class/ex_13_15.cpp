9de67123dac8a853a158e4c7968b9f197790ea55
Alexander Kobez <alexander.kobez@gmail.com>
2022-06-13 12:48:02 +0300

    Create ex_13_15.cpp

---
+++ 13 - Stream Input-Output. A Deeper Look/13.15 - Point Class/ex_13_15.cpp
@@ -0,0 +1,19 @@
+// Exercise 13.15: ex_13_15.cpp
+// Point Class
+
+#include <iostream>
+#include "Point.h"
+
+using namespace std;
+
+int main() {
+
+   Point point;
+
+   cout << "Coordinates: ";
+   cin >> point;
+
+   cout << point << endl;
+
+   return 0;
+}
