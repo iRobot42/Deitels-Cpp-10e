5bd081db594dbb587045d0b42e5951e1bb42a1fd
Alexander Kobez <alexander.kobez@gmail.com>
2022-07-04 22:33:42 +0300

    Create ex_13_16.cpp

---
+++ 13 - Stream Input-Output. A Deeper Look/13.16 - Complex Class/ex_13_16.cpp
@@ -0,0 +1,19 @@
+// Exercise 13.16: ex_13_16.cpp
+// Complex Class
+
+#include <iostream>
+#include "Complex.h"
+
+using namespace std;
+
+int main() {
+
+   Complex number;
+
+   cout << "Complex number (e.g. 3 + 8i): ";
+   cin >> number;
+
+   cout << number << endl;
+
+   return 0;
+}
