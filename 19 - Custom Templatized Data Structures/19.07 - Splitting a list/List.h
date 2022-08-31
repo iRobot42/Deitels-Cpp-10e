6cbe60ea85b5f521f9dd8663a87f8df98804a452
Alexander Kobez <alexander.kobez@gmail.com>
2022-08-31 10:39:28 +0300

    Add files via upload

---
+++ 19 - Custom Templatized Data Structures/19.07 - Splitting a list/List.h
@@ -0,0 +1,37 @@
+// Exercise 19.6: List.h
+
+#ifndef LIST_H
+#define LIST_H
+
+#include <iostream>
+
+struct Node {
+   char data{};
+   Node* next{};
+};
+
+struct List {
+
+   Node* head{}; // not safe,
+   Node* tail{}; // but enough
+
+   void add( const char C ) {
+      Node* temp = new Node;
+      temp->data = C;
+      if ( head ) {
+         tail->next = temp;
+         tail = tail->next;
+      } else head = tail = temp;
+   }
+
+   void print( const std::string& INFO ) const {
+      std::cout << INFO << ": ";
+      if ( head ) {
+         Node* current{ head };
+         do std::cout << current->data << ' ';
+         while ( current = current->next );
+      } else std::cout << "empty";
+   }
+};
+
+#endif
