4e1d327ccb5243bd1a3d7525c886f2e15ec77821
Alexander Kobez <alexander.kobez@gmail.com>
2022-09-03 13:58:48 +0300

    Add files via upload

---
+++ 19 - Custom Templatized Data Structures/19.12 - Infix-to-Postfix Conversion/Stack.h
@@ -0,0 +1,53 @@
+// Exercise 19.12: Stack.h
+
+#ifndef STACK_H
+#define STACK_H
+
+template< typename Type >
+class Stack {
+
+   struct Node {
+      Type data{ NULL };
+      Node* next{};
+      explicit Node( const Type& DATA ) : data{ DATA } {};
+   };
+
+   Node* head{};
+
+public:
+
+   ~Stack() {
+
+      Node* temp{};
+      while ( head ) {
+         temp = head;
+         head = head->next;
+         delete temp;
+      }
+   }
+
+   void push( const Type& DATA ) {
+
+      Node* temp{ new Node( DATA ) };
+      if ( head ) {
+         temp->next = head;
+         head = temp;
+      }
+      else head = temp;
+   }
+
+   Type pop() {
+
+      if ( !head ) return Type( NULL );
+      Node* temp{ head };
+      head = head->next;
+      const Type DATA{ temp->data };
+      delete temp;
+      return DATA;
+   }
+
+   Type top() { return head->data; }
+   bool empty() const { return !head; }
+};
+
+#endif
