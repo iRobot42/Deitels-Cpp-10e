44af9ff414133d32c924567d7c3e42a73d4b911c
Alexander Kobez <alexander.kobez@gmail.com>
2022-09-06 10:03:22 +0300

    Add files via upload

---
+++ 19 - Custom Templatized Data Structures/19.19 - Depth of a Binary Tree/Tree.h
@@ -0,0 +1,72 @@
+// Exercise 19.19: Tree.h
+
+#ifndef TREE_H
+#define TREE_H
+
+template< typename Type >
+class Tree {
+
+   struct Node {
+
+      Type data{};
+      Node* left{};
+      Node* right{};
+
+      explicit Node( const Type& DATA ) : data{ DATA } {}
+   };
+
+   Node* root{};
+
+   void insertHelper( Node** ptr, const Type& DATA ) {
+
+      if ( !*ptr )
+         *ptr = new Node( DATA );
+      else if ( DATA < ( *ptr )->data )
+         insertHelper( &( ( *ptr )->left ), DATA );
+      else if ( DATA > ( *ptr )->data )
+         insertHelper( &( ( *ptr )->right ), DATA );
+      else std::cout << "\nDuplicate: " << DATA;
+   }
+
+   void preorderHelper( Node* ptr ) const {
+
+      if ( !ptr ) return;
+      std::cout << ptr->data << ' ';
+      preorderHelper( ptr->left );
+      preorderHelper( ptr->right );
+   }
+
+   void inorderHelper( Node* ptr ) const {
+
+      if ( !ptr ) return;
+      inorderHelper( ptr->left );
+      std::cout << ptr->data << ' ';
+      inorderHelper( ptr->right );
+   }
+
+   void postorderHelper( Node* ptr ) const {
+
+      if ( !ptr ) return;
+      postorderHelper( ptr->left );
+      postorderHelper( ptr->right );
+      std::cout << ptr->data << ' ';
+   }
+
+   int calculateDepthHelper( Node* ptr ) const {
+
+      if ( !ptr ) return Type();
+      int lDepth{ calculateDepthHelper( ptr->left ) };
+      int rDepth{ calculateDepthHelper( ptr->right ) };
+      return lDepth > rDepth ? ++lDepth : ++rDepth;
+   }
+
+public:
+
+   void insert( const Type& DATA ) { insertHelper( &root, DATA ); }
+   void preorderTraversal() const  { preorderHelper( root ); }
+   void inorderTraversal() const   { inorderHelper( root ); }
+   void postorderTraversal() const { postorderHelper( root ); }
+   int calculateDepth() const      { return calculateDepthHelper( root ); }
+};
+
+#endif
