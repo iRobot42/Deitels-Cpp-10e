cef846d5db1888cd43466b663249d907c44f0690
Alexander Kobez <alexander.kobez@gmail.com>
2023-03-12 08:28:54 +0300

    Create ex_22_05.cpp

---
+++ 22 - Bits, Characters, C Strings and structs/22.05 - Card Shuffling and Dealing/ex_22_05.cpp
@@ -0,0 +1,13 @@
+// Exercise 22.5: ex_22_05.cpp
+// Card Shuffling and Dealing
+
+#include "DeckOfCards.h"
+
+int main() {
+
+   DeckOfCards deckOfCards;
+   deckOfCards.shuffle();
+   deckOfCards.deal();
+
+   return 0;
+}
