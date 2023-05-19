fe09c96e19cfd22cd8a226ab13c8fbed8f9dfe6e
Alexander Kobez <alexander.kobez@gmail.com>
2023-05-19 22:24:44 +0300

    Create ex_E_08.cpp

---
+++ E - Preprocessor/E08/ex_E_08.cpp
@@ -0,0 +1,14 @@
+// Exercise E.8: ex_E_08.cpp
+
+#include <iostream>
+using namespace std;
+
+#define PRINT( s ) cout << s << endl;
+
+int main() {
+
+   const string test{ "Lorem ipsum dolor sit amet" };
+   PRINT( test );
+
+   return EXIT_SUCCESS;
+}
